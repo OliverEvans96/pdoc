335265ce9bfa3c6582a76842636209ff94658886
Oliver Evans <oliverevans96@gmail.com>
2023-02-16 13:41:25 -0800

    Switch from hex to base32 encoding for id

--- src/id.rs
+++ src/id.rs
@@ -1,11 +1,10 @@
 use std::fmt::{Debug, Display};
 
-use hex::ToHex;
 use rand::{distributions::Standard, prelude::Distribution, Rng};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Copy, Deserialize, Serialize)]
-pub struct Id([u8; 8]);
+pub struct Id([u8; 10]);
 
 impl Distribution<Id> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Id {
@@ -15,7 +14,8 @@ impl Distribution<Id> for Standard {
 
 impl Display for Id {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s: String = self.0.encode_hex();
+        // let s: String = self.0.encode_hex();
+        let s = base32::encode(base32::Alphabet::Crockford, &self.0).to_lowercase();
         f.write_str(&s)
     }
 }
@@ -30,7 +30,7 @@ impl Debug for Id {
 mod tests {
     use rand::{thread_rng, Rng};
 
-    use crate::Id;
+    use super::Id;
 
     #[test]
     fn test_rand_id() {
