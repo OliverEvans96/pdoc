3701a2ca15a315d429a9a6da5afd16ec8f0e6f1c
Oliver Evans <oliverevans96@gmail.com>
2023-02-16 14:36:38 -0800

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 
+use clap::{Parser, Subcommand};
+
 use crate::{client::Client, contact::ContactInfo, invoice::Invoice};
 
 mod address;
@@ -13,20 +15,47 @@ mod price;
 mod project;
 mod storage;
 
-// TODO integrate inquire (questions cli)
-// TODO nested create (invoice, project, client)
-fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+#[derive(Subcommand)]
+enum Command {
+    /// Get or create client
+    Client,
+}
 
-    let client = Client::create_from_user_input();
+#[derive(Parser)]
+struct Opts {
+    #[command(subcommand)]
+    command: Command,
+}
+
+fn get_or_create_client() -> anyhow::Result<()> {
+    let client = Client::create_from_user_input()?;
 
     println!("got client: {:#?}", client);
 
+    client.save()?;
+
+    Ok(())
+}
+
+fn generate_invoice() -> anyhow::Result<()> {
     // let invoice_file = File::open("invoice.yaml")?;
     // let invoice: Invoice = serde_yaml::from_reader(invoice_file)?;
     // let full_invoice = invoice.collect()?;
 
     // full_invoice.render_pdf("out.pdf")?;
+
+    Ok(())
+}
+
+// TODO nested create (invoice, project, client)
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+
+    println!("Hello!");
+
+    match opts.command {
+        Command::Client => get_or_create_client()?,
+    }
 
     println!("Done!");
 
